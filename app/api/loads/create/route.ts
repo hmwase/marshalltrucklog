117716647e0b176dd9984efef5f0d87dfbf477f3
AissaSemaoui <semaoui0347@gmail.com>
2024-01-12 23:08:57 +0100

    destroy load when it's checked in

--- app/api/loads/create/route.ts
+++ app/api/loads/create/route.ts
@@ -1,20 +1,41 @@
-
 import TruckCheckIn from "@/app/lib/models/TruckCheckIn";
+import Load from "@/app/lib/models/load";
 
 export async function POST(request: Request) {
   try {
     const body = await request.json();
     console.log("Received data:", body);
+    const { loadId, ...payload } = body;
 
-    const truckCheckIn = await TruckCheckIn.create(body);
+    const truckCheckIn = await TruckCheckIn.create(payload);
+
+    if (loadId) {
+      const response = await Load.destroy({
+        where: {
+          LoadId: loadId,
+        },
+      });
+      console.log(response);
+    }
 
     console.log("Data inserted into the database:", truckCheckIn.toJSON());
 
     // Respond with a success message and status code 200
-    return new Response(JSON.stringify({ truckCheckInId: truckCheckIn.truckCheckInId, destinationState: truckCheckIn.destinationState, driversName:truckCheckIn.driversName,location: truckCheckIn.location, trailerType: truckCheckIn.trailerType, pickupNumber: truckCheckIn.pickupNumber, company: truckCheckIn.company}), {
-      headers: { "Content-Type": "application/json" },
-      status: 200,
-    });
+    return new Response(
+      JSON.stringify({
+        truckCheckInId: truckCheckIn.truckCheckInId,
+        destinationState: truckCheckIn.destinationState,
+        driversName: truckCheckIn.driversName,
+        location: truckCheckIn.location,
+        trailerType: truckCheckIn.trailerType,
+        pickupNumber: truckCheckIn.pickupNumber,
+        company: truckCheckIn.company,
+      }),
+      {
+        headers: { "Content-Type": "application/json" },
+        status: 200,
+      }
+    );
   } catch (error) {
     console.error("Error inserting data into the database:", error);
 
